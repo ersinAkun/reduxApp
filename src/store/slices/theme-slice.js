75502d551ed7e452f04c929c2d9759e511fc6f1f
Ersin Akun <ersinakun34@gmail.com>
2023-01-21 21:15:46 +0300

    v2

---
+++ src/store/slices/theme-slice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+export const themeSlice = createSlice({
+    name: "theme",
+    initialState:{
+        darkMode: false           
+    },
+    reducers:{
+        setDarkMode: (state, action) => {
+            state.darkMood = action.payload;
+        }        
+    }
+});
+
+export const {setDarkMode} = themeSlice.actions; 
+
+
