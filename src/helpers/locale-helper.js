ba2a7846efde4f502617d4f6e21772b645a97bb5
Ersin Akun <ersinakun34@gmail.com>
2023-01-21 22:48:59 +0300

    v7

--- src/helpers/locale-helper.js
+++ src/helpers/locale-helper.js
@@ -1,11 +1,5 @@
-
-
 export const langs = [
     {title: "English", code: "en", country: "gb"},
-    {title: "Turkce", code: "tr", country: "tr"},
+    {title: "Türkçe", code: "tr", country: "tr"},
     {title: "Francais", code: "fr", country: "fr"},
-
 ]
-
-
-
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { localeSlice } from "./slices/localeslice";
+
 import { themeSlice } from "./slices/theme-slice";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "flag-icons/css/flag-icons.min.css";
+import { localeSlice } from "./slices/locail.-slice";
 
 export default configureStore({
     reducer: {
